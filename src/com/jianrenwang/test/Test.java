24327cd5a8b7e59b24f314fd32924dd1ceaf51bc
XY <540104231@QQ.COM>
2014-06-18 22:13:23 +0800

    env update,change webContent to webRoot, update settings

--- src/com/jianrenwang/test/Test.java
+++ src/com/jianrenwang/test/Test.java
@@ -1,8 +1,12 @@
 package com.jianrenwang.test;
 
+import javax.annotation.Resource;
+
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import com.jianrenwang.data.pojo.Job;
+import com.jianrenwang.data.service.JobService;
 import com.jianrenwang.utils.Ut;
 
 public class Test {
